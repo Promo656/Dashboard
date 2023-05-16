64cae4768fa3a2d254d5471c93bea940b18a8167
egor <rank2007@mail.ru>
2023-05-16 12:11:00 +0300

    set up project

---
+++ webpack.config.ts
@@ -0,0 +1,46 @@
+const path = require("path")
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+    entry: "./src/index.tsx",
+    mode:"development",
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, "dist"),
+    },
+    target: "web",
+    devtool: 'inline-source-map',
+    devServer: {
+        port: '3000',
+        static: {
+            directory: path.join(__dirname, 'public')
+        },
+        open: true,
+        hot: true,
+        liveReload: true,
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".json"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            },
+        ],
+    },
+    plugins: [
+        // new BundleAnalyzerPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'public', 'index.html')
+        })
+    ]
+}
